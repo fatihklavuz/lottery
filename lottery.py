2d0bf0815cf80fdc3d71d0b7fc3150586ea30a00
fatihklavuz <64463795+fatihklavuz@users.noreply.github.com>
2020-06-27 12:08:24 +0300

    Update and rename loto.py to lottery.py

--- lottery.py
+++ lottery.py
@@ -10,7 +10,6 @@ while a>=0:
 	b4=random.randint(1,49)
 	b5=random.randint(1,49)
 	b6=random.randint(1,49)
-	print(str(b1)+" "+str(b2)+" "+str(b3)+" "+ str(b4) + " "+ str(b5) + " "+str(b6) + "\n")
 	w=open("loto.txt","a")
 	w.write(str(b1)+" "+str(b2)+" "+str(b3)+" "+ str(b4) + " "+ str(b5) + " "+str(b6) + "\n")
 	w.close()	
